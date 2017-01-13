945a0398ae607368ccbf9c6f73372f685120ecbe
Ganaraja <ganaraja@gmail.com>
2017-01-13 11:41:53 +0530

    Assignment 11 - Optional

--- app/scripts/filters/timecode.js
+++ app/scripts/filters/timecode.js
@@ -4,19 +4,9 @@
           var seconds = Number.parseFloat(seconds);
           if (Number.isNaN(seconds)) {
               return '-:--';
-          }          
-          var wholeSeconds = Math.floor(seconds);
-          var minutes = Math.floor(wholeSeconds / 60);
-          var remainingSeconds = wholeSeconds % 60;
-
-          var output = minutes + ':';
-
-          if (remainingSeconds < 10) {
-              output += '0';
           }
-
-          output += remainingSeconds;
-            return output;
+          timer = buzz.toTimer(seconds)
+          return timer;
         };
     }
 
